9c662aeca6f9b9df02293a6d188f556d78d441cb
RonanBelleil <77842372+RonanBelleil@users.noreply.github.com>
2021-10-25 16:28:38 +0200

    Rectification des dimensions de FLAIR

--- FlairUnreordered.py
+++ FlairUnreordered.py
@@ -8,13 +8,10 @@ from flair.embeddings import WordEmbeddings, FlairEmbeddings, StackedEmbeddings
 from flair.data import Sentence
 
 
-glove_embedding = WordEmbeddings('glove')
-
 flair_embedding_forward = FlairEmbeddings('fr-forward')
 flair_embedding_backward = FlairEmbeddings('fr-backward')
 
 stacked_embeddings = StackedEmbeddings([
-                                        glove_embedding,
                                         flair_embedding_forward,
                                         flair_embedding_backward,
                                        ])
@@ -26,15 +23,6 @@ for i in range(len(mess)):
     embeds.append(stc)
 
 print(mess)
-
-"""
-for sentence in embeds:
-    for token in sentence:
-        print(token)
-        print(token.embedding)
-"""
-print("embeds[0] token : ", embeds[0])
-print("embeds[0][0] token embedding: ", embeds[0][0].embedding)
 
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
@@ -49,12 +37,12 @@ for i in range(len(embeds)):
 
 Y = mess.iloc[:,1]
 classif = svm.SVC(kernel='linear')
-print(X[0])
+print("Dimension des vecteurs = ", len(X[0]))
 
 tab = cross_val_score(classif, X, Y, cv=10, scoring="f1")
-print(tab)
 
 f1 = 0
 for i in range(len(tab)):
+  print("f1[", i, "] = ", tab[i])
   f1 = f1 + tab[i]
-print(f1/len(tab))
+print("moyenne des f1 scores = ", f1/len(tab))
