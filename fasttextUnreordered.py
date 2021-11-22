e8ad62f0b8121968cd26f14124d7345f61ba7c37
karfouilla <seraphin.costa@free.fr>
2021-11-22 16:57:01 +0100

    Update fasttextUnreordered

--- fasttextUnreordered.py
+++ fasttextUnreordered.py
@@ -1,6 +1,4 @@
 # config :
-# - dataset : messages_equilibre.csv
-# - modification : aucune
 # - modèle de représentation des mots : fasttext, cc.fr.300.bin
 # - modèle de séparation des mots : fasttext (par espace)
 # - modèle de classification : scikitlearn, SVM
@@ -17,17 +15,20 @@ import sklearn.metrics
 import numpy as np
 import numpy.linalg
 import random
-import spacy
+import argparse
 
 def sentenseVector(model, sentense):
 	return model.get_sentence_vector(sentense)
 
-dt = pd.read_table("messages_equilibre_ok.csv", header=None, sep=',', encoding="utf-8")
+parser = argparse.ArgumentParser(description='Approche fasttext avec la tache de classification avec SVM')
+parser.add_argument("input", help="Fichier du dataset")
+args = parser.parse_args()
+
+dt = pd.read_table(args.input, header=None, sep=',', encoding="utf-8")
 ft = fasttext.load_model('cc.fr.300.bin')
-nlp = spacy.load("fr_core_news_lg")
 X = pd.DataFrame([sentenseVector(ft, str(msg).replace('\n', ' ')) for msg in dt.iloc[:,0].tolist()])
 y = dt.iloc[:,1]
 clf = svm.SVC(kernel='linear', C=1, random_state=42)
 results = cross_val_score(clf, X, y, cv=10, scoring="f1")
-results
-np.mean(results)
+print("F-score de la validation croisée : ", results)
+print("Moyenne des F-score", np.mean(results))
